dcd7e75e0ac03bd4c2e284318036a8a18d2b1685
nenuchepanu001 <137167809+nenuchepanu001@users.noreply.github.com>
2023-08-31 18:12:26 +0530

    Update 3_create_external_file_format.sql

--- ldw/3_create_external_file_format.sql
+++ ldw/3_create_external_file_format.sql
@@ -30,3 +30,37 @@ IF NOT EXISTS ( SELECT * FROM sys.external_file_formats WHERE name='csv_file_for
             , PARSER_VERSION = '1.0'
         )
     );
+
+
+-- tsv file formt 2.0
+
+IF NOT EXISTS ( SELECT * FROM sys.external_file_formats WHERE name='tsv_file_format')
+    CREATE EXTERNAL FILE FORMAT tsv_file_format
+    WITH
+    (
+        FORMAT_TYPE=DELIMITEDTEXT,
+        FORMAT_OPTIONS (
+            FIELD_TERMINATOR = '\t'
+            , STRING_DELIMITER = '"'
+            , FIRST_ROW = 2 -- ONLY AVAILABLE FOR AZURE SYNAPSE ANALYTICS
+            , USE_TYPE_DEFAULT = FALSE
+            , ENCODING ='UTF8'
+            , PARSER_VERSION = '2.0'
+        )
+    );
+
+
+IF NOT EXISTS ( SELECT * FROM sys.external_file_formats WHERE name='tsv_file_format_pv1')
+    CREATE EXTERNAL FILE FORMAT tsv_file_format_pv1
+    WITH
+    (
+        FORMAT_TYPE=DELIMITEDTEXT,
+        FORMAT_OPTIONS (
+            FIELD_TERMINATOR = '\t'
+            , STRING_DELIMITER = '"'
+            , FIRST_ROW = 2 -- ONLY AVAILABLE FOR AZURE SYNAPSE ANALYTICS
+            , USE_TYPE_DEFAULT = FALSE
+            , ENCODING ='UTF8'
+            , PARSER_VERSION = '1.0'
+        )
+    );
