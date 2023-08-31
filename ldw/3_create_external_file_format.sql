ef3c9c7c9911bd1dea54bad031eb744249a82d05
nenuchepanu001 <137167809+nenuchepanu001@users.noreply.github.com>
2023-08-31 17:09:37 +0530

    Update 3_create_external_file_format.sql

--- ldw/3_create_external_file_format.sql
+++ ldw/3_create_external_file_format.sql
@@ -1,15 +1,32 @@
 USE nyc_taxi_ldw;
 
-CREATE EXTERNAL FILE FORMAT csv_file_format
-WITH
-(
-    FORMAT_TYPE=DELIMITEDTEXT,
-    FORMAT_OPTIONS (
-        FIELD_TERMINATOR = ','
-        , STRING_DELIMITER = '"'
-        , FIRST_ROW = 2 -- ONLY AVAILABLE FOR AZURE SYNAPSE ANALYTICS
-        , USE_TYPE_DEFAULT = FALSE
-        , ENCODING ='UTF8'
-        , PARSER_VERSION = '2.0'
-    )
-);
+IF NOT EXISTS ( SELECT * FROM sys.external_file_formats WHERE name='csv_file_format')
+    CREATE EXTERNAL FILE FORMAT csv_file_format
+    WITH
+    (
+        FORMAT_TYPE=DELIMITEDTEXT,
+        FORMAT_OPTIONS (
+            FIELD_TERMINATOR = ','
+            , STRING_DELIMITER = '"'
+            , FIRST_ROW = 2 -- ONLY AVAILABLE FOR AZURE SYNAPSE ANALYTICS
+            , USE_TYPE_DEFAULT = FALSE
+            , ENCODING ='UTF8'
+            , PARSER_VERSION = '2.0'
+        )
+    );
+
+
+IF NOT EXISTS ( SELECT * FROM sys.external_file_formats WHERE name='csv_file_format_pv1')
+    CREATE EXTERNAL FILE FORMAT csv_file_format_pv1
+    WITH
+    (
+        FORMAT_TYPE=DELIMITEDTEXT,
+        FORMAT_OPTIONS (
+            FIELD_TERMINATOR = ','
+            , STRING_DELIMITER = '"'
+            , FIRST_ROW = 2 -- ONLY AVAILABLE FOR AZURE SYNAPSE ANALYTICS
+            , USE_TYPE_DEFAULT = FALSE
+            , ENCODING ='UTF8'
+            , PARSER_VERSION = '1.0'
+        )
+    );
