ec0f37b2a8d0108c89426a92d142ad47d120b386
nenuchepanu001 <137167809+nenuchepanu001@users.noreply.github.com>
2023-08-31 17:09:18 +0530

    Update 2_create_external_data_sources.sql

--- ldw/2_create_external_data_sources.sql
+++ ldw/2_create_external_data_sources.sql
@@ -1,7 +1,8 @@
 USE nyc_taxi_ldw;
 
-CREATE EXTERNAL DATA SOURCE nyc_taxi_src
-WITH
-(
-    LOCATION='https://synapse006coursedl.dfs.core.windows.net/nyc-taxi-data'
-);
+IF NOT EXISTS (SELECT * FROM sys.external_data_sources WHERE name='nyc_taxi_src')
+    CREATE EXTERNAL DATA SOURCE nyc_taxi_src
+    WITH
+    (
+        LOCATION='https://synapse006coursedl.dfs.core.windows.net/nyc-taxi-data'
+    );
